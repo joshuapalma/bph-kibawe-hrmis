14c87e0674f19f8a811af9d6c8c7f86bf0a185ae
joshuapalma <joshuapalma57@gmail.com>
2022-10-26 23:51:17 +0800

    commit update verify csrf token

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,9 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        'login',
+        'register',
+        'reset-password',
+        'change-password'
     ];
 }
